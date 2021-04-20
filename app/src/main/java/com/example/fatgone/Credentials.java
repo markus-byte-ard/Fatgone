944b59fe6c0e6275cbc6e4388e1f6b3cc1a65ece
markus-byte-ard <markustaulu@gmail.com>
2021-04-20 19:08:37 +0300

    Registeration Done with multiple users

---
+++ app/src/main/java/com/example/fatgone/Credentials.java
@@ -0,0 +1,30 @@
+package com.example.fatgone;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Credentials {
+    HashMap<String, String> credentialsMapper = new HashMap<String, String>();
+
+    public void addCredentials (String username, String password) {
+        credentialsMapper.put(username, password);
+    }
+    public boolean checkUsername (String username) {
+        return credentialsMapper.containsKey(username);
+    }
+    public boolean verifyCredentials (String username, String password) {
+        if (credentialsMapper.containsKey(username)){
+            if (password.equals(credentialsMapper.get(username))){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void loadCredentials (Map<String, ?> preferencesMap) {
+        for (Map.Entry<String, ?> entries : preferencesMap.entrySet()) {
+            if (!entries.getKey().equals("RememberMeCheckBox")) {
+                credentialsMapper.put(entries.getKey(), entries.getValue().toString());
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fatgone/RegistrationActivity.java
@@ -0,0 +1,86 @@
+package com.example.fatgone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Map;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText eRegName;
+    private EditText eRegPassword;
+    private Button eRegister;
+
+    public Credentials credentials;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor sharedPreferencesEditor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        eRegName = findViewById(R.id.etRegName);
+        eRegPassword = findViewById(R.id.etRegPassword);
+        eRegister = findViewById(R.id.btnRegister);
+
+        credentials = new Credentials();
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("CredentialsDB", MODE_PRIVATE);
+        sharedPreferencesEditor = sharedPreferences.edit();
+
+        if(sharedPreferences != null){
+
+            Map<String, ?> preferencesMap = sharedPreferences.getAll();
+
+            if(preferencesMap.size() != 0){
+                credentials.loadCredentials(preferencesMap);
+            }
+        }
+
+        eRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String regUsername = eRegName.getText().toString();
+                String regPassword = eRegPassword.getText().toString();
+
+                if(validate(regUsername, regPassword)) {
+
+                    if(credentials.checkUsername(regUsername)){
+                        Toast.makeText(RegistrationActivity.this, "Username already taken!", Toast.LENGTH_SHORT).show();
+                    }else{
+
+                        credentials.addCredentials(regUsername, regPassword);
+
+                        sharedPreferencesEditor.putString(regUsername, regPassword);
+                        sharedPreferencesEditor.putString("LastSavedUsername", "");
+                        sharedPreferencesEditor.putString("LastSavedPassword", "");
+                        sharedPreferencesEditor.apply();
+
+                        startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+                    }
+                }
+            }
+        });
+
+    }
+
+    private boolean validate(String username, String password){
+
+        if(username.isEmpty() || password.length() < 8){
+            Toast.makeText(this, "Please enter all the details, password should be atleast 8 characters!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+}

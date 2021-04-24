c2ac42d021eb191d6489d32c53529dea1b66afad
EmilioPizzuti <pizzuti.dev@gmail.com>
2021-04-24 14:20:04 +0300

    Merged everything into MainActivity

--- app/src/main/java/com/example/fatgone/FragmentHome.java
+++ app/src/main/java/com/example/fatgone/FragmentHome.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,15 +16,7 @@ public class FragmentHome extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
-
-        /*
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                firebase.savecurUser(curUser);
-            }
-        });*/
-        //return super.onCreateView(inflater, container, savedInstanceState);
         return view;
     }
+
 }

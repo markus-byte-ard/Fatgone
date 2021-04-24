5cad2f99cf66113596d6af283bade107a466c9a0
EmilioPizzuti <pizzuti.dev@gmail.com>
2021-04-24 15:42:42 +0300

    Method streamlining and cleaning

--- app/src/main/java/com/example/fatgone/FragmentProfile.java
+++ app/src/main/java/com/example/fatgone/FragmentProfile.java
@@ -90,16 +90,18 @@ public class FragmentProfile extends Fragment {
             @Override
             public void onClick(View v) {
                 if (!editWeightInput.getText().toString().isEmpty()) {
-
                     argWeight = Double.parseDouble(editWeightInput.getText().toString());
                     yourWeight.setText("Weight: " + argWeight + " kg");
-                    argBmi = (argWeight / ((argHeight / 100) * (argHeight / 100)));
-                    argBmi = (Math.round(argBmi * 100.0) / 100.0);
-                    yourBmi.setText("BMI: " + String.valueOf(argBmi) + " kg/m^2");
-                    getWeight();
+                    updateBmi();
                 }
             }
         });
+    }
+
+    private void updateBmi() {
+        argBmi = (argWeight / ((argHeight / 100) * (argHeight / 100)));
+        argBmi = (Math.round(argBmi * 100.0) / 100.0);
+        yourBmi.setText("BMI: " + String.valueOf(argBmi) + " kg/m^2");
     }
 
     public double getWeight(){
